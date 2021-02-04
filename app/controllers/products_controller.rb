c6739cd8d5f8ab67faea8012646d046d0780a97a
Bruno Genovez <genovezb@gmail.com>
2021-02-04 20:37:27 -0300

    created products controler

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,32 @@
+class ProductsController < ApplicationController
+
+  def index
+    @product = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product), notice: 'Product create!'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, photos:[])
+  end
+
+
+end

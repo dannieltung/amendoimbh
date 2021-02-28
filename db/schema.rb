77564aca3612515f397a0ef359c6f6fdef1cfec2
Danniel Tung <dwftung@gmail.com>
2021-02-28 20:53:11 -0300

    refactoring budgets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_221907) do
+ActiveRecord::Schema.define(version: 2021_02_28_235000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_221907) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.bigint "product_id"
+    t.integer "quantity"
     t.index ["product_id"], name: "index_budgets_on_product_id"
     t.index ["user_id"], name: "index_budgets_on_user_id"
   end
@@ -29,7 +30,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_221907) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "price"
     t.string "name"
-    t.integer "quantity"
+    t.integer "stock_quantity"
   end
 
   create_table "users", force: :cascade do |t|

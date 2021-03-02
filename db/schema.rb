d633dbe34515f49a48421a11a2626ab46b577586
Appiopini <appiopini@gmail.com>
2021-03-02 00:39:27 -0300

    removed spaces

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2021_02_28_235000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "budgets", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
